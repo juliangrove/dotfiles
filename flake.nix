6729c993fc00bf6303450bd75b59648bcf00f72d
Julian Grove <julian.grove@gmail.com>
2021-08-15 03:56:49 -0400

    Update flake.nix.

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
 
   outputs = { self, nixpkgs }: {
 
-    nixosConfigurations.container = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.nixthinkpad = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules =
         [ ./configuration.nix ];
