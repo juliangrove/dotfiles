5113407ead9621e545f79512b4b9e1c19be9b08e
Julian Grove <julian.grove@gmail.com>
2021-12-15 01:22:23 +0100

    Update to 21.11.

--- configuration.nix
+++ configuration.nix
@@ -210,7 +210,6 @@
 
   # fonts
   fonts = {
-    fontconfig.dpi = 145;
     fonts = with pkgs; [
       corefonts
       emacs-all-the-icons-fonts
@@ -243,5 +242,5 @@
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
   # should.
-  system.stateVersion = " 21.05 "; # Did you read the comment?
+  system.stateVersion = " 21.11 "; # Did you read the comment?
 }
